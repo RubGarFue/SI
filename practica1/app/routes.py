2d542f5b4ef213e2019a773178f4572464e37f55
RubGarFue <RubGarFue@hotmail.com>
2021-10-18 22:12:17 +0200

    shopping_cart funcional

--- practica1/app/routes.py
+++ practica1/app/routes.py
@@ -76,6 +76,9 @@ def login():
             message = "La contraseña no es correcta"
             return render_template('login.html', title="Videoclub - Iniciar sesión", message=message)
         
+        # Hacemos que la sea propia del usuario
+        _user_shopping_cart(username)
+
         session['usuario'] = username
         session.modified = True
 
@@ -90,7 +93,7 @@ def login():
 @app.route('/logout', methods=['GET', 'POST'])
 def logout():
     session.pop('usuario', None)
-    session.pop('shopping_cart')
+    #session.pop('shopping_cart')
     session.modified = True
     return redirect(url_for('index'))
 
@@ -126,16 +129,24 @@ def register():
     hash.update(password.encode('utf8'))
     file.write('{0}'.format(hash.hexdigest()) + '\n')
 
-    file.write(email + '\n' + credit_card + '\n')
+    file.write(email + '\n' + direction + '\n' + credit_card + '\n')
     file.write(str(random.randint(0, 100)))
 
     file.write('\n0')
 
     file.close()
 
+    # Creamos un historial json
     file = open('app/usuarios/' + username + '/historial.json', 'w')
     
     file.write('{\n\t\"compras\": []\n}')
+
+    file.close()
+
+    # Creamos una cesta de la compra json
+    file = open('app/usuarios/' + username + '/shopping_cart.json', 'w')
+    
+    file.write('{\n\t\"articulos\": []\n}')
 
     file.close()
     
@@ -151,11 +162,38 @@ def movie(movie_id):
     if request.method == "POST":
         if request.form['add-to-cart'] == 'add':
             units = int(request.form['units'])
+            
+            # Leemos el json
+            file_data = _read_shopping_cart()
+            
+            # Si la pelicula esta actualizamos unidades
+            new_film = True
+            for articulo in file_data['articulos']:
+                if articulo['titulo'] == peli['titulo']:
+                    articulo['cantidad'] += units
+                    new_film = False
+                    break
+            
+            # Si no añadimos nueva entrada
+            if new_film:
+                articulo = {'titulo': peli['titulo'],
+                            'poster': peli['poster'],
+                            'cantidad': units,
+                            'precio_u': peli['precio']}
+                
+                file_data['articulos'].append(articulo)
+            
+            # Escribimos el json
+            _write_shopping_cart(file_data)
+
+            '''
+            units = int(request.form['units'])
             if session.get('shopping_cart'):
                 session['shopping_cart'][movie_id] = session['shopping_cart'].get(movie_id,0) + units
             else:
                 session['shopping_cart'] = {movie_id: units}
             session.modified=True
+            '''
     return render_template('movie.html', title="Videoclub - " + peli["titulo"], movie=peli)
 
 
@@ -190,6 +228,20 @@ def history():
 @app.route('/shopping-cart', methods=['GET', 'POST'])
 def shopping_cart():
     catalogue = get_movies()
+    
+    # Leemos el json
+    file_data = _read_shopping_cart()
+
+    # Si no hay articulos devolvemos cesta vacia
+    if not file_data['articulos']:
+        return render_template('shopping-cart.html', message="")
+    
+    # Si hay articulos los ponemos en my_movies
+    my_movies = []
+    for articulo in file_data['articulos']:
+        my_movies.append(articulo)
+    
+    '''
     if not session.get('shopping_cart'):
         return render_template('shopping-cart.html', message="")
     my_movies = []
@@ -197,6 +249,7 @@ def shopping_cart():
         movie = catalogue[int(movie_ind)]
         movie['unidades'] = units
         my_movies.append(movie)
+    '''
     
     if request.method == 'GET':
         return render_template('shopping-cart.html', products=my_movies, message="")
@@ -212,12 +265,22 @@ def shopping_cart():
         
         precio_total = 0
         
+        shopping_cart_data = _read_shopping_cart()
+
+        for articulo in shopping_cart_data['articulos']:
+            precio = float(articulo['precio_u'])
+            uds = int(articulo['cantidad'])
+
+            precio_total += precio * uds
+
+        '''
         for movie_id, items in session['shopping_cart'].items():
             
             precio = movie['precio']
             uds = items
 
             precio_total += precio*uds
+        '''
             
         if saldo < precio_total:
             message = "No tiene el saldo suficiente para realizar la compra"
@@ -231,10 +294,29 @@ def shopping_cart():
         fecha = date.today()
         fecha = fecha.strftime('%d/%m/%Y')
 
-        if fecha not in file_data['compras'][-1].values():
+        try:
+            if fecha not in file_data['compras'][-1].values():
+                file_data['compras'].append({"fecha": fecha, "articulos": []})
+        except IndexError:
             file_data['compras'].append({"fecha": fecha, "articulos": []})
 
-        #escribimos cada artículo
+        # escribimos cada artículo
+        # articulo representa un articulo del shopping_cart y movie_hist una peliucla del historial
+        for articulo in shopping_cart_data['articulos']:
+            new_film = True
+
+            for movie_hist in file_data['compras'][-1]['articulos']:
+                if articulo['titulo'] == movie_hist['titulo']:
+                    movie_hist['cantidad'] += articulo['cantidad']
+                    new_film = False
+                    break
+            
+            if not new_film:
+                continue
+
+            file_data['compras'][-1]['articulos'].append(articulo)
+        
+        '''
         for movie_id , items in session['shopping_cart'].items():
             nueva_peli = 1
 
@@ -255,6 +337,7 @@ def shopping_cart():
 
             articulo = {"titulo": titulo, "poster": poster, "cantidad": uds, "precio_u": precio}
             file_data['compras'][-1]['articulos'].append(articulo)
+        '''
         
         file.close()
         
@@ -270,17 +353,91 @@ def shopping_cart():
 
         with open('app/usuarios/' + session['usuario'] + '/data.dat', 'w') as file_dat:
             file_dat.writelines(data)
+        
+        # Vaciamos el historial
+        file = open('app/usuarios/' + session['usuario'] + '/shopping_cart.json', 'w')
+    
+        file.write('{\n\t\"articulos\": []\n}')
+
+        file.close()
 
         # Vaciamos la shoopping_cart
-        session.pop('shopping_cart')
-        session.modified = True
+        #session.pop('shopping_cart')
+        #session.modified = True
 
         return redirect(url_for('index'))
 
 
-@app.route('/shopping-cart/update/<int:movie_id>', methods=['GET', 'POST'])
-def shopping_cart_update(movie_id):
+def _user_shopping_cart(user):
+    
+    # Movemos el json a la carpeta de usuario
+    path_src = 'app/shopping_cart/shopping_cart.json'
+    path_dst = 'app/usuarios/' + user + '/shopping_cart.json'
+
+    # Comprobamos si la cesta sin usuario no esta vacia
+    if _read_shopping_cart()['articulos']:
+        os.replace(path_src, path_dst)
+
+        # Creamos un nuevo json
+        file = open('app/shopping_cart/shopping_cart.json', 'w')
+        
+        file.write('{\n\t\"articulos\": []\n}')
+
+        file.close()
+    return
+
+def _read_shopping_cart():
+    if 'usuario' not in session:
+        path = 'app/shopping_cart/shopping_cart.json'
+    else:
+        path = 'app/usuarios/' + session['usuario'] + '/shopping_cart.json'
+
+    with open(path, 'r+') as file:
+        file_data = json.load(file)
+
+    return file_data
+
+def _write_shopping_cart(file_data):
+    if 'usuario' not in session:
+        path = 'app/shopping_cart/shopping_cart.json'
+    else:
+        path = 'app/usuarios/' + session['usuario'] + '/shopping_cart.json'
+
+    with open(path, 'w') as file:
+        json.dump(file_data, file)
+
+    return
+
+@app.route('/shopping-cart/update/<string:titulo>', methods=['GET', 'POST'])
+def shopping_cart_update(titulo):
+    #!! Seria mejor cambiar el argumento de entrada movie_id por titulo
+    #for pelicula in get_movies():
+    #    if pelicula['id'] == movie_id:
+    #        titulo = pelicula['titulo']
+
     if request.method == "POST":
+        # Leemos el json
+        file_data = _read_shopping_cart()
+
+        if request.form['update'] == 'update':
+            units = int(request.form['units'])
+            
+            for articulo in file_data['articulos']:
+                if articulo['titulo'] == titulo:
+                    articulo['cantidad'] = units
+                    break
+
+        elif request.form['update'] == 'remove':
+            for articulo in file_data['articulos']:
+                if articulo['titulo'] == titulo:
+                    file_data['articulos'].remove(articulo)
+                    break
+        
+        # Escribimos el json
+        _write_shopping_cart(file_data)
+
+        return redirect(url_for('shopping_cart'))
+        '''
         if request.form['update'] == 'update':
             units = int(request.form['units'])
             session['shopping_cart'][movie_id] = units
@@ -289,6 +446,7 @@ def shopping_cart_update(movie_id):
             session['shopping_cart'].pop(movie_id)
             session.modified=True
         return redirect(url_for('shopping_cart'))
+        '''
 
 
 def get_movies():
