8b1a0499090915557b4e624060afc438270d98cf
RubGarFue <RubGarFue@hotmail.com>
2021-10-20 16:48:50 +0200

    final commit

--- practica1/app/routes.py
+++ practica1/app/routes.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 from app import app
-from flask import render_template, request, url_for, redirect, session, flash
+from flask import render_template, request, url_for, redirect, session
 from hashlib import blake2b
 from datetime import date
 import json
@@ -12,10 +12,10 @@ import random
 
 
 @app.route('/')
-@app.route('/index', methods=['GET','POST'])
+@app.route('/index', methods=['GET', 'POST'])
 def index():
     print(url_for('static', filename='css/style.css'), file=sys.stderr)
-    
+
     catalogue = get_movies()
 
     # CARGAR CATEGORIAS DISPONIBLES
@@ -29,23 +29,29 @@ def index():
         if request.form['search-button'] == 'search':
             # search bar
             text = request.form['search-text']
-            catalogue = [mov for mov in catalogue if text.lower() in mov['titulo'].lower()]
+            catalogue = [mov for mov in catalogue if text.lower()
+                         in mov['titulo'].lower()]
 
             # filter genre
             genre = request.form.get('filter')
-            if genre: 
-                catalogue = [mov for mov in catalogue if genre in mov['categoria']]
+            if genre:
+                catalogue = [
+                    mov for mov in catalogue if genre in mov['categoria']]
 
             # update subtitle
             subtitle = "Resultados"
             if text:
-                subtitle += " para '" + text  + "'" 
+                subtitle += " para '" + text + "'"
             if genre:
-                subtitle += " de la categoría " + genre.lower() 
+                subtitle += " de la categoría " + genre.lower()
 
-            return render_template('index.html', title="VIDEOCLUB", subtitle=subtitle, genres=genres, movies=catalogue)
+            return render_template('index.html', title="VIDEOCLUB",
+                                   subtitle=subtitle, genres=genres,
+                                   movies=catalogue)
 
-    return render_template('index.html', subtitle="Cartelera actual", genres=genres, movies=catalogue)
+    return render_template(
+        'index.html', subtitle="Cartelera actual", genres=genres,
+        movies=catalogue)
 
 
 @app.route('/login', methods=['GET'])
@@ -55,7 +61,7 @@ def show_login():
 
 @app.route('/login', methods=['POST'])
 def login():
-    message=""
+    message = ""
     salt = "laurayrubensonlosmejores"
 
     username = request.form['username']
@@ -63,7 +69,9 @@ def login():
 
     password += salt
 
+    # Comprobamos que el usuario introducido existe en la carpeta "usuarios"
     if os.path.isdir('app/usuarios/' + username):
+        # Comprobamos que la contraseña sea correcta
         hash = blake2b()
         hash.update(password.encode('utf8'))
         password = '{0}'.format(hash.hexdigest())
@@ -74,26 +82,30 @@ def login():
 
         if password != password_check:
             message = "La contraseña no es correcta"
-            return render_template('login.html', title="Videoclub - Iniciar sesión", message=message)
-        
+            return render_template(
+                'login.html', title="Videoclub - Iniciar sesión",
+                message=message)
+
         # Hacemos que la sea propia del usuario
         _user_shopping_cart(username)
 
+        # Modificamos la sesión y volvemos a index
         session['usuario'] = username
         session.modified = True
 
         return redirect(url_for('index'))
-    
+
     else:
+        # Si no existe mesnaje de error y retornamos
         message = "No existe el usuario introducido"
 
-        return render_template('login.html', title="Videoclub - Iniciar sesión", message=message)
+        return render_template(
+            'login.html', title="Videoclub - Iniciar sesión", message=message)
 
 
 @app.route('/logout', methods=['GET', 'POST'])
 def logout():
     session.pop('usuario', None)
-    #session.pop('shopping_cart')
     session.modified = True
     return redirect(url_for('index'))
 
@@ -105,7 +117,7 @@ def show_register():
 
 @app.route('/register', methods=['POST'])
 def register():
-    message=""
+    message = ""
     salt = "laurayrubensonlosmejores"
 
     username = request.form['username']
@@ -116,12 +128,16 @@ def register():
 
     password += salt
 
+    # Comprobamos si el usuario ya existe en la carpeta "usuarios"
     if os.path.isdir('app/usuarios/' + username):
         message = "El usuario introducido ya existe"
-        return render_template('register.html', title="Videoclub - Registro", message=message)
+        return render_template(
+            'register.html', title="Videoclub - Registro", message=message)
 
+    # Creamos una carpeta con el nombre del usuario
     os.mkdir('app/usuarios/' + username)
 
+    # Escribimos todos los campos en el data.dat
     file = open('app/usuarios/' + username + '/data.dat', 'w')
     file.write(username + '\n')
 
@@ -138,19 +154,20 @@ def register():
 
     # Creamos un historial json
     file = open('app/usuarios/' + username + '/historial.json', 'w')
-    
+
     file.write('{\n\t\"compras\": []\n}')
 
     file.close()
 
     # Creamos una cesta de la compra json
     file = open('app/usuarios/' + username + '/shopping_cart.json', 'w')
-    
+
     file.write('{\n\t\"articulos\": []\n}')
 
     file.close()
-    
-    return render_template('login.html', title="Videoclub - Iniciar sesión", message=message)
+
+    return render_template(
+        'login.html', title="Videoclub - Iniciar sesión", message=message)
 
 
 @app.route('/movie/<int:movie_id>', methods=['GET', 'POST'])
@@ -162,10 +179,10 @@ def movie(movie_id):
     if request.method == "POST":
         if request.form['add-to-cart'] == 'add':
             units = int(request.form['units'])
-            
+
             # Leemos el json
             file_data = _read_shopping_cart()
-            
+
             # Si la pelicula esta actualizamos unidades
             new_film = True
             for articulo in file_data['articulos']:
@@ -173,39 +190,32 @@ def movie(movie_id):
                     articulo['cantidad'] += units
                     new_film = False
                     break
-            
+
             # Si no añadimos nueva entrada
             if new_film:
                 articulo = {'titulo': peli['titulo'],
                             'poster': peli['poster'],
                             'cantidad': units,
                             'precio_u': peli['precio']}
-                
+
                 file_data['articulos'].append(articulo)
-            
+
             # Escribimos el json
             _write_shopping_cart(file_data)
-
-            '''
-            units = int(request.form['units'])
-            if session.get('shopping_cart'):
-                session['shopping_cart'][movie_id] = session['shopping_cart'].get(movie_id,0) + units
-            else:
-                session['shopping_cart'] = {movie_id: units}
-            session.modified=True
-            '''
-    return render_template('movie.html', title="Videoclub - " + peli["titulo"], movie=peli)
+    return render_template(
+        'movie.html', title="Videoclub - " + peli["titulo"], movie=peli)
 
 
 @app.route('/history', methods=['GET', 'POST'])
 def history():
     if 'usuario' not in session:
-        return render_template('login.html', title="Videoclub - Iniciar sesión", message="")
+        return render_template(
+            'login.html', title="Videoclub - Iniciar sesión", message="")
 
     # Update data.dat (saldo)
     with open('app/usuarios/' + session['usuario'] + '/data.dat', 'r') as file:
         data = file.readlines()
-    
+
     saldo = float(data[-2])
     puntos = data[-1]
 
@@ -213,70 +223,55 @@ def history():
     if request.method == "POST":
         if request.form['add-balance-button'] == 'add':
             amount = float(request.form['add-balance'])
-            saldo = str(round(saldo + amount,2))
+            saldo = str(round(saldo + amount, 2))
             data[-2] = saldo + '\n'
             data[-1] = puntos
 
-            with open('app/usuarios/' + session['usuario'] + '/data.dat', 'w') as file:
+            with open('app/usuarios/' + session['usuario'] + '/data.dat',
+                      'w') as file:
                 file.writelines(data)
 
     history = get_history(session['usuario'])
-    return render_template('history.html', saldo=saldo, puntos=puntos, compras=history)
+    return render_template('history.html', saldo=saldo,
+                           puntos=puntos, compras=history)
 
 
 @app.route('/shopping-cart', methods=['GET', 'POST'])
 def shopping_cart():
-    catalogue = get_movies()
-    
     # Leemos el json
     file_data = _read_shopping_cart()
 
     # Si no hay articulos devolvemos cesta vacia
     if not file_data['articulos']:
         return render_template('shopping-cart.html', message="")
-    
+
     # Si hay articulos los ponemos en my_movies
     my_movies = []
     for articulo in file_data['articulos']:
         my_movies.append(articulo)
-    
-    '''
-    if not session.get('shopping_cart'):
-        return render_template('shopping-cart.html', message="")
-    my_movies = []
-    for movie_ind, units in session['shopping_cart'].items():
-        movie = catalogue[int(movie_ind)]
-        movie['unidades'] = units
-        my_movies.append(movie)
-    '''
-    
+
     if request.method == 'GET':
-        return render_template('shopping-cart.html', products=my_movies, message="")
-    
+        return render_template('shopping-cart.html',
+                               products=my_movies, message="")
+
     if request.method == 'POST':
-        #comprobamos si estamos logeados
+        # comprobamos si estamos logeados
         if 'usuario' not in session:
-            return render_template('login.html', title="Videoclub - Iniciar sesión", message="")
-        
+            return render_template(
+                'login.html', title="Videoclub - Iniciar sesión", message="")
+
         # Obtenemos el precio total de la compra
         shopping_cart_data = _read_shopping_cart()
 
+        # Calculamos el precio total de los artículos
         precio_total = 0
         for articulo in shopping_cart_data['articulos']:
             precio = float(articulo['precio_u'])
             uds = int(articulo['cantidad'])
             precio_total += precio * uds
 
-        '''
-        for movie_id, items in session['shopping_cart'].items():
-            
-            precio = movie['precio']
-            uds = items
-
-            precio_total += precio*uds
-        '''
-
-        with open('app/usuarios/' + session['usuario'] + '/data.dat', 'r') as file:
+        with open('app/usuarios/' + session['usuario'] + '/data.dat',
+                  'r') as file:
             data = file.readlines()
             saldo = float(data[-2][:-1])
             puntos = int(data[-1])
@@ -284,16 +279,20 @@ def shopping_cart():
         method = request.form['payment-method']
         if method == "puntos":
             # Comprobamos si el usuario tiene puntos suficientes
-            if puntos < round(precio_total*100,2):
-                message = "No tiene el suficientes puntos para realizar la compra (100 puntos equivalen a 1e)"
-                return render_template('shopping-cart.html', products=my_movies, message=message)
+            if puntos < round(precio_total * 100, 2):
+                message = "No tiene los puntos suficientes para realizar la c"\
+                          "ompra (100 puntos equivalen a 1e)"
+                return render_template(
+                    'shopping-cart.html', products=my_movies, message=message)
             new_saldo = saldo
-            new_puntos = int(puntos - precio_total*100)
+            new_puntos = int(puntos - precio_total * 100)
         elif method == "saldo":
             # Comprobamos si el usuario tiene el saldo suficiente
             if saldo < precio_total:
-                message = "No tiene el saldo suficiente para realizar la compra"
-                return render_template('shopping-cart.html', products=my_movies, message=message)
+                message = "No tiene el saldo suficiente para realizar la comp"\
+                          "ra"
+                return render_template(
+                    'shopping-cart.html', products=my_movies, message=message)
             new_saldo = saldo - precio_total
             new_puntos = puntos
 
@@ -301,10 +300,14 @@ def shopping_cart():
         new_puntos += int(0.05 * precio_total)
 
         # Abrimos el archivo json
-        file = open('app/usuarios/' + session['usuario'] + '/historial.json', 'r+')
+        file = open(
+            'app/usuarios/' +
+            session['usuario'] +
+            '/historial.json',
+            'r+')
         file_data = json.load(file)
 
-        #comprobamos si ya se han hecho compras durante el día
+        # comprobamos si ya se han hecho compras durante el día
         fecha = date.today()
         fecha = fecha.strftime('%d/%m/%Y')
 
@@ -315,7 +318,8 @@ def shopping_cart():
             file_data['compras'].append({"fecha": fecha, "articulos": []})
 
         # escribimos cada artículo
-        # articulo representa un articulo del shopping_cart y movie_hist una peliucla del historial
+        # articulo representa un articulo del shopping_cart y movie_hist una
+        # peliucla del historial
         for articulo in shopping_cart_data['articulos']:
             new_film = True
 
@@ -324,66 +328,46 @@ def shopping_cart():
                     movie_hist['cantidad'] += articulo['cantidad']
                     new_film = False
                     break
-            
+
             if not new_film:
                 continue
 
             file_data['compras'][-1]['articulos'].append(articulo)
-        
-        '''
-        for movie_id , items in session['shopping_cart'].items():
-            nueva_peli = 1
 
-            movie = get_movies()[int(movie_id)]
-            titulo = movie['titulo']
-            poster = movie['poster']
-            precio = movie['precio']
-            uds = items
-
-            for articulo in file_data['compras'][-1]['articulos']:
-                if titulo in articulo.values():
-                    articulo['cantidad'] += uds
-                    nueva_peli = 0
-                    break
-            
-            if nueva_peli == 0:
-                continue
-
-            articulo = {"titulo": titulo, "poster": poster, "cantidad": uds, "precio_u": precio}
-            file_data['compras'][-1]['articulos'].append(articulo)
-        '''
-        
         file.close()
-        
-        with open('app/usuarios/' + session['usuario'] + '/historial.json', 'w') as file:
+
+        with open('app/usuarios/' + session['usuario'] + '/historial.json',
+                  'w') as file:
             json.dump(file_data, file)
 
         # Actualizamos el data.dat (saldo y puntos)
-        with open('app/usuarios/' + session['usuario'] + '/data.dat', 'r') as file_dat:
+        with open('app/usuarios/' + session['usuario'] + '/data.dat',
+                  'r') as file_dat:
             data = file_dat.readlines()
-        
+
         data[-2] = str(round(new_saldo, 2)) + '\n'
         data[-1] = str(new_puntos)
 
-        with open('app/usuarios/' + session['usuario'] + '/data.dat', 'w') as file_dat:
+        with open('app/usuarios/' + session['usuario'] + '/data.dat',
+                  'w') as file_dat:
             file_dat.writelines(data)
-        
+
         # Vaciamos el historial
-        file = open('app/usuarios/' + session['usuario'] + '/shopping_cart.json', 'w')
-    
+        file = open(
+            'app/usuarios/' +
+            session['usuario'] +
+            '/shopping_cart.json',
+            'w')
+
         file.write('{\n\t\"articulos\": []\n}')
 
         file.close()
-
-        # Vaciamos la shoopping_cart
-        #session.pop('shopping_cart')
-        #session.modified = True
 
         return redirect(url_for('index'))
 
 
 def _user_shopping_cart(user):
-    
+
     # Movemos el json a la carpeta de usuario
     path_src = 'app/shopping_cart/shopping_cart.json'
     path_dst = 'app/usuarios/' + user + '/shopping_cart.json'
@@ -394,11 +378,12 @@ def _user_shopping_cart(user):
 
         # Creamos un nuevo json
         file = open('app/shopping_cart/shopping_cart.json', 'w')
-        
+
         file.write('{\n\t\"articulos\": []\n}')
 
         file.close()
     return
+
 
 def _read_shopping_cart():
     if 'usuario' not in session:
@@ -411,6 +396,7 @@ def _read_shopping_cart():
 
     return file_data
 
+
 def _write_shopping_cart(file_data):
     if 'usuario' not in session:
         path = 'app/shopping_cart/shopping_cart.json'
@@ -422,20 +408,16 @@ def _write_shopping_cart(file_data):
 
     return
 
+
 @app.route('/shopping-cart/update/<string:titulo>', methods=['GET', 'POST'])
 def shopping_cart_update(titulo):
-    #!! Seria mejor cambiar el argumento de entrada movie_id por titulo
-    #for pelicula in get_movies():
-    #    if pelicula['id'] == movie_id:
-    #        titulo = pelicula['titulo']
-
     if request.method == "POST":
         # Leemos el json
         file_data = _read_shopping_cart()
 
         if request.form['update'] == 'update':
             units = int(request.form['units'])
-            
+
             for articulo in file_data['articulos']:
                 if articulo['titulo'] == titulo:
                     articulo['cantidad'] = units
@@ -446,21 +428,11 @@ def shopping_cart_update(titulo):
                 if articulo['titulo'] == titulo:
                     file_data['articulos'].remove(articulo)
                     break
-        
+
         # Escribimos el json
         _write_shopping_cart(file_data)
 
         return redirect(url_for('shopping_cart'))
-        '''
-        if request.form['update'] == 'update':
-            units = int(request.form['units'])
-            session['shopping_cart'][movie_id] = units
-            session.modified=True
-        elif request.form['update'] == 'remove':
-            session['shopping_cart'].pop(movie_id)
-            session.modified=True
-        return redirect(url_for('shopping_cart'))
-        '''
 
 
 def get_movies():
@@ -472,40 +444,12 @@ def get_movies():
 
 def get_history(user):
     history_data = open(os.path.join(
-        app.root_path, 'usuarios/' + user + '/historial.json'), encoding="utf-8").read()
+        app.root_path, 'usuarios/' + user + '/historial.json'),
+        encoding="utf-8").read()
     history = json.loads(history_data)
     return history['compras']
 
 
 @app.route('/numusers')
 def numusers():
-    return str(random.randint(1,100))
-
-
-'''
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    # doc sobre request object en http://flask.pocoo.org/docs/1.0/api/#incoming-request-data
-    if 'username' in request.form:
-        # aqui se deberia validar con fichero .dat del usuario
-        if request.form['username'] == 'pp':
-            session['usuario'] = request.form['username']
-            session.modified=True
-            # se puede usar request.referrer para volver a la pagina desde la que se hizo login
-            return redirect(url_for('index'))
-        else:
-            # aqui se le puede pasar como argumento un mensaje de login invalido
-            return render_template('login.html', title = "Sign In")
-    else:
-        # se puede guardar la pagina desde la que se invoca 
-        session['url_origen']=request.referrer
-        session.modified=True        
-        # print a error.log de Apache si se ejecuta bajo mod_wsgi
-        print (request.referrer, file=sys.stderr)
-        return render_template('login.html', title = "Sign In")
-
-@app.route('/logout', methods=['GET', 'POST'])
-def logout():
-    session.pop('usuario', None)
-    return redirect(url_for('index'))
-'''
+    return str(random.randint(1, 100))
