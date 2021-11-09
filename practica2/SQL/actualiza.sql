491e1ed37e44bf0f0309d1d1ddf1d4b4327bef2d
RubGarFue <RubGarFue@hotmail.com>
2021-11-09 23:47:56 +0100

    más consultas (todas comprobadas)

--- practica2/SQL/actualiza.sql
+++ practica2/SQL/actualiza.sql
@@ -99,5 +99,63 @@ $$ LANGUAGE plpgsql;
 SELECT setCustomersBalance(100);
 
 
+-- c) procedimiento setOrderAmount
+-- TODO: No se si se puede poner todo en un mismo update (por los WHERE * IS NULL)
+CREATE OR REPLACE FUNCTION setOrderAmount() RETURNS void as $$
+BEGIN
+    UPDATE public.orders
+    SET netamount = TOTAL.total
+    FROM
+        (SELECT orders.orderid, SUM(orderdetail.price) AS total
+        FROM public.orders, public.orderdetail
+        WHERE orders.orderid = orderdetail.orderid
+        GROUP BY orders.orderid) AS TOTAL
+    WHERE orders.orderid = TOTAL.orderid
+    AND netamount IS NULL;
+
+    UPDATE public.orders
+    SET totalamount = netamount + tax
+    WHERE totalamount IS NULL;
+END;
+$$ LANGUAGE plpgsql;
+
+-- Llamada al procedimiento 'setOrderAmount'
+SELECT setOrderAmount();
+
+
 -- d) procedimiento getTopSales
-CREATE OR REPLACE FUNCTION getTopSales(year1 INT, year2 INT, OUT Year INT, OUT Film CHAR, OUT sales bigint);
+CREATE OR REPLACE FUNCTION getTopSales(year1 INT, year2 INT, OUT Year INT, OUT Film CHAR, OUT sales bigint) as $$
+BEGIN
+-- !! EL UNICO PROBLEMA DE ESTA FUNCION ES QUE AL HACER EL OUT Y AL MOSTRAR EL RESULTADO DE LA FUNCION SOLO DA LA PRIMERA
+-- !! COLUMNA, PERO EL SEGUNDO SELECT SÍ QUE DA UNA TABLA CON LAS PELICULAS MAS COMPRADAS ENTRE DOS AÑOS DADOS
+    CREATE VIEW moviecount
+    AS
+
+    SELECT movietitle, year, COUNT(*) AS count
+    FROM
+        (SELECT imdb_movies.movietitle, DATE_PART('year', orders.orderdate) as year
+        FROM public.orders INNER JOIN public.orderdetail
+        ON orders.orderid = orderdetail.orderid
+        INNER JOIN public.products
+        ON products.prod_id = orderdetail.prod_id
+        INNER JOIN public.imdb_movies
+        ON imdb_movies.movieid = products.movieid) AS foo
+    GROUP BY movietitle, year;
+
+    SELECT moviecount.year::INT, movietitle, max
+    INTO Year, Film, sales
+    FROM
+        (SELECT moviecount.year, MAX(count) AS max
+        FROM moviecount
+        GROUP BY moviecount.year) AS moviemax
+    INNER JOIN moviecount
+    ON moviecount.year = moviemax.year
+    AND moviecount.count = moviemax.max
+
+    WHERE moviemax.year BETWEEN year1 AND year2
+
+    ORDER BY max DESC;
+
+    DROP VIEW moviecount;
+END;
+$$ LANGUAGE plpgsql;
--- practica2/SQL/setPrice.sql
+++ practica2/SQL/setPrice.sql
@@ -1,2 +1,7 @@
--- incrementar precio 2% anual desde orderdate
--- actual * 1.02^(ahora - entonxes)
+-- b) consulta setPrice
+-- el precio que tenemos que poner es el precio original, así que la fórmula es:
+-- precio_original = precio_actual / 1.02^(fecha_actual - fecha_entonces)
+UPDATE public.orderdetail
+SET price = ROUND(CAST(products.price / POW(1.02, DATE_PART('year', NOW()::date) - DATE_PART('year', orders.orderdate::date)) AS NUMERIC), 2)
+FROM public.orders, public.products
+WHERE orderdetail.orderid = orders.orderid AND orderdetail.prod_id = products.prod_id;
