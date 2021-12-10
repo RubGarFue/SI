58a224a9b25418134ab7bde16d8cdaac2b834356
RubGarFue <RubGarFue@hotmail.com>
2021-12-10 15:41:22 +0100

    ciudadesDistintas

--- practica3/SQL/ciudadesDistintas.sql
+++ practica3/SQL/ciudadesDistintas.sql
@@ -1,7 +1,7 @@
+-- Mejor índice con formato YYYYMM
+
 CREATE INDEX idx_year
 ON orders(orderdate);
-		  
-CREATE INDEX idx_date ON orders (EXTRACT (year FROM orderdate), EXTRACT(month FROM orderdate));
 
 EXPLAIN ANALYZE
 SELECT COUNT(DISTINCT city)
@@ -11,11 +11,16 @@ ON orders.customerid = customers.customerid
 WHERE creditcardtype = 'VISA'
 AND orderdate = TO_DATE('201604', 'YYYYMM');
 
+
+-- Consulta más óptima cambiando un poco la forma de la misma
+
+CREATE INDEX idx_date ON orders (EXTRACT (year FROM orderdate), EXTRACT(month FROM orderdate));
+
 EXPLAIN ANALYZE
 SELECT COUNT(DISTINCT city)
 FROM customers
 INNER JOIN orders
 ON orders.customerid = customers.customerid
 WHERE creditcardtype = 'VISA'
-AND EXTRACT (year FROM orderdate ) = 2016
-AND EXTRACT (MONTH FROM orderdate ) = 4;
+AND EXTRACT (year FROM orderdate) = 2016
+AND EXTRACT (MONTH FROM orderdate) = 4;
