e67622a2069541fb01673a6a021c424f3d1dc177
RubGarFue <RubGarFue@hotmail.com>
2021-12-17 22:23:09 +0100

    Final commit

--- practica3/SQL/updPromo.sql
+++ practica3/SQL/updPromo.sql
@@ -29,24 +29,3 @@ CREATE TRIGGER updPromo_trigger
 AFTER UPDATE OF promo ON customers
 FOR EACH ROW
 EXECUTE PROCEDURE updPromo();
-
-
--- ESTO NO VA AQUI, ES SIMPLEMENTE PARA EJECUTAR EL APARTADO I
-UPDATE orders
-SET status = NULL
-WHERE orderid = 1
-OR orderid = 7
-OR orderid = 23;
-
-
-ERROR:  deadlock detected
-DETAIL:  Process 15050 waits for ShareLock on transaction 11512; blocked by process 16314.
-Process 16314 waits for ShareLock on transaction 11521; blocked by process 15050.
-HINT:  See server log for query details.
-CONTEXT:  while updating tuple (1686,19) in relation "orders"
-SQL statement "UPDATE orders
-    SET netamount = ROUND(CAST(netamount - netamount * 0.01 * NEW.promo AS NUMERIC), 2)
-    WHERE customerid = NEW.customerid
-    AND status IS NULL"
-PL/pgSQL function updpromo() line 7 at SQL statement
-SQL state: 40P01
